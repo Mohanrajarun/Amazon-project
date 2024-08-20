a89db72eb447425d4d8d3c8d077cb6abb78965de
Mohanrajarun <mohanrajarun6@gmail.com>
2024-08-21 02:36:39 +0530

    Delete javascript directory

--- javascript/script.js
+++
@@ -1,7 +0,0 @@
-const btn = document.getElementsByClassName('hamburger')[0];
-const navbar = document.getElementsByClassName('hamburger-menu')[0];
-
-
-btn.addEventListener('click',()=>{
-    navbar.classList.toggle('active');
-});
